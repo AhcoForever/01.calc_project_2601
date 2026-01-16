6dd1d2158f824a3eef3a0a2a01be399563f236cf
miny <ahco8766@kakao.com>
2026-01-16 10:01:41 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,15 +1,30 @@
+# 더하기 함수
 def sum(num1, num2):
     return num1 + num2
 
 
-def sub (num1, num2):
-    return num1-num2
+# 빼기 함수
+def sub(num1, num2):
+    return num1 - num2
 
-def div (num1, num2):
-    if num2 == 0 :
+
+# 나누기 함수
+def div(num1, num2):
+    if num2 == 0:
         return 0
     return num1 / num2
 
-def mul (num1, num2):
+
+# 곱하기 함수
+def mul(num1, num2):
     return num1 * num2
 
+
+# 제곱 함수
+def square(a):
+    return a**2
+
+
+# 나머지 함수
+def mod(a, b):
+    return a % b
